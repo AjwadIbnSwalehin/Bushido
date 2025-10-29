33f973f2580af8579a975163c669b5f6fb6ffa9d
Ajwad <aibnswalehin@gmail.com>
2025-10-29 02:56:02 +0000

    Instance testing from vscode using rojo

---
+++ src/server/ChangePart.server.lua
@@ -0,0 +1,5 @@
+local myPart = game.Workspace.Part -- gets the part
+myPart.Color = Color3.new(1, 0, 0) -- makes it red
+myPart.Transparency = 0.3 -- slightly see-through
+myPart.Anchored = true -- locks it in place
+print("Part updated!")
